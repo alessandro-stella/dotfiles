4939061be9846314a495a4bcdaf808e2023cf9e8
alessandro-stella <alessandro.stella2004@gmail.com>
2026-06-27 13:38:34 +0200

    Removed dynamic files

--- hypr/dynamic-border.lua
+++
@@ -1,9 +0,0 @@
--- Dynamic border color converted from dynamic-border.conf
-
-hl.config({
-	general = {
-		col = {
-			active_border = { colors = { "rgba(DEEFEFee)", "rgba(D1B6BAee)" }, angle = 45 },
-		},
-	},
-})
